54e75e4d56eb592642778256bbf66d94c9a9f972
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-08-06 09:13:18 -0600

    Update style and script file
    - Test some Js features with scope

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// Scoping
+function calcAge(birthYear) {
+  const age = 2040 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    // const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1991 % birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Victor';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'New Output!!!';
+    }
+    console.log(str);
+    console.log(millenial);
+    console.log(add(2, 3));
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Gabriel';
+calcAge(1997);
+// console.log(age);
+// printAge();
